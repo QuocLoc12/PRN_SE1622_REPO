2bb282b079d31619f3e360b4c3ccf055a0776820
ASUS <ASUS@NGUYENLOC>
2022-05-19 15:03:12 +0700

    Add project files.

---
+++ PRN_SE1622_PRO/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace fptedu.se1622;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($"Hello, World....");
+
+        int x = 10, y = 20;
+
+        Console.WriteLine($"Total of a = {x} and b = {y} is: {Sum(x,y)}");
+        Console.ReadLine();
+    }
+
+    public static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+}
